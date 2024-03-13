1f6e8a56320c1f1469ef9e5489c8818ffb0a2c10
RAF81437 <raf81vales@gmail.com>
2024-03-13 11:52:54 +0100

    added new file

---
+++ Hello_word2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    // Print "Hello, World!" to the console
+    printf("Bye Bye, World!\n");
+
+    // Return 0 to indicate successful completion
+    return 0;
+}
